e0a06ee84e2ecfc78eb65ed280b71194e07245d8
wentaowanguc <wentao.wang.us@gmail.com>
2026-07-24 17:04:22 -0700

    Add solution for palindrome-number

---
+++ palindrome-number/is-palindrome-number.py
@@ -0,0 +1,21 @@
+class IsPalindromeNumber:
+    def is_palindrome_with_str_function(self, x: int) -> bool:
+        if x < 0 :
+            return False
+        
+        return str(x) == str(x)[::-1]
+        
+
+    def is_palindrome_mathmatics(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        
+        reversed_number = 0
+        original_number = x
+
+        while original_number > 0:
+            digit = original_number % 10
+            reversed_number = reversed_number * 10 + digit
+            original_number //= 10
+
+        return reversed_number == x
